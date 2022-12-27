985340f3203f098ecdd1b6135a0eab6723ba7408
Charles Marques <charles_sjp@yahoo.com.br>
2022-12-27 18:36:09 -0300

    implement logic buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,23 +7,26 @@ import Ordenador from "./Ordenador";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
-  const [filtro, setFiltro] = useState<number | null>(null)
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
   return (
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>Full Stack Developer</div>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
       </header>
       <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
-          <Filtros  filtro={filtro} setFiltro={setFiltro}/>
-          <Ordenador/>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
       </section>
     </main>
-  );
+  )
 }
