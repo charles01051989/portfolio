ccbd868909726a2641e8353d4dc01aec1e1a0261
Charles Marques <charles_sjp@yahoo.com.br>
2023-01-05 15:19:04 -0300

    implements routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Cardapio from "./pages/Cardapio";
 import "normalize.css";
 import Inicio from "pages/Inicio";
+import Cardapio from "pages/Cardapio";
+import Router from './routes'
+
+const componenteAtual =
+  window.location.pathname === "/" ? <Inicio /> : <Cardapio />;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Inicio />
+    <Router/>
   </React.StrictMode>
 );
