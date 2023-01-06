80639cc8dd3c669f0b0b2d564937923acf814807
Charles Marques <charles_sjp@yahoo.com.br>
2023-01-05 22:02:03 -0300

    add page Sobre

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,24 @@
-import Cardapio from 'pages/Cardapio'
-import Inicio from 'pages/Inicio'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Cardapio from "pages/Cardapio";
+import Inicio from "pages/Inicio";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "components/Menu";
+import PaginaPadrao from "components/PaginaPadrao";
+import Sobre from "pages/Sobre";
 
-export default function AppRouter(){
-    return(
-        <Router>
-            <Routes>
-                <Route path='/' element={<Inicio/>}/>
-                <Route path='/Cardapio' element={<Cardapio/>}/>
-            </Routes>
-        </Router>
-    )
+export default function AppRouter() {
+  return (
+    <main>
+      <Router>
+        <Menu />
+
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
+  );
 }
