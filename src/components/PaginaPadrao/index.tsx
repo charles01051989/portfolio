6ee6e1ce3593112c7391c256a6fb8fa529fbfe02
Charles Marques <charles_sjp@yahoo.com.br>
2023-01-06 21:14:22 -0300

    implement NotFound page

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,15 +1,17 @@
-import styles from "./PaginaPadrao.module.scss";
-import { Outlet } from "react-router-dom";
-import stylesTema from 'styles/Tema.module.scss'
+import styles from './PaginaPadrao.module.scss';
+import { Outlet } from 'react-router-dom';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function PaginaPadrao() {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.header__text}>A casa do código e da massa</div>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
       </header>
       <div className={stylesTema.container}>
-        <Outlet/>
+        <Outlet />
       </div>
     </>
   );
